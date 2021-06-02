4319dbafeb820d8eec7f8b4fe74db9faa46c3a36
itcast <946917394@qq.com>
2021-06-03 00:07:44 +0800

    four commit

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,22 +4,20 @@
       <div slot="center">购物街</div>
     </nar-bar>
 
-    <home-swiper
-      :banners="banners.list"
-    ></home-swiper>
+    <home-swiper :banners="bannersList"></home-swiper>
   </div>
 </template>
 
 <script>
-import NarBar from "components/common/navbar/NavBar"
-import HomeSwiper from "views/home/childcomps/HomeSwiper"
+import NarBar from "components/common/navbar/NavBar";
+import HomeSwiper from "views/home/childcomps/HomeSwiper";
 
-import {getHomeMultidata} from 'network/home'
+import { getHomeMultidata } from "network/home";
 export default {
   name: "Home",
   components: {
     NarBar,
-    HomeSwiper
+    HomeSwiper,
   },
   data() {
     return {
@@ -33,12 +31,10 @@ export default {
             acm: "3.mce.2_10_1jhwa.43542.0.ccy5br4OlfK0Q.pos_0-m_454801-sd_119",
             height: 390,
             height923: 390,
-            image:
-              "https://www.baidu.com/img/flexible/logo/pc/result.png",
+            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
             image923:
               "https://s10.mogucdn.com/mlcdn/c45406/180926_7d5c521e0aa3h38786lkakebkjlh8_750x390.jpg",
-            link:
-              "https://www.baidu.com",
+            link: "https://www.baidu.com",
             title: "焕新女装节",
             width: 750,
             width923: 750,
@@ -47,12 +43,10 @@ export default {
             acm: "3.mce.2_10_1ji16.43542.0.ccy5br4OlfK0R.pos_1-m_454889-sd_119",
             height: 390,
             height923: 390,
-            image:
-              "https://www.baidu.com/img/flexible/logo/pc/result.png",
+            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
             image923:
               "https://s10.mogucdn.com/mlcdn/c45406/180926_14l41d2ekghbeh771g3ghgll54224_750x390.jpg",
-            link:
-              "https://www.baidu.com",
+            link: "https://www.baidu.com",
             title: "入秋穿搭指南",
             width: 750,
             width923: 750,
@@ -61,12 +55,10 @@ export default {
             acm: "3.mce.2_10_1jfj8.43542.0.ccy5br4OlfK0S.pos_2-m_453270-sd_119",
             height: 390,
             height923: 390,
-            image:
-              "https://www.baidu.com/img/flexible/logo/pc/result.png",
+            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
             image923:
               "https://s10.mogucdn.com/mlcdn/c45406/180919_47iclhel8f4ld06hid21he98i93fc_750x390.jpg",
-            link:
-              "https://www.baidu.com",
+            link: "https://www.baidu.com",
             title: "秋季护肤大作战",
             width: 750,
             width923: 750,
@@ -75,12 +67,10 @@ export default {
             acm: "3.mce.2_10_1jepe.43542.0.ccy5br4OlfK0T.pos_3-m_452733-sd_119",
             height: 390,
             height923: 390,
-            image:
-              "https://www.baidu.com/img/flexible/logo/pc/result.png",
+            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
             image923:
               "https://s10.mogucdn.com/mlcdn/c45406/180917_0hgle1e2c350a57ekhbj4f10a6b03_750x390.jpg",
-            link:
-              "https://www.baidu.com",
+            link: "https://www.baidu.com",
             title: "流行抢先一步",
             width: 750,
             width923: 750,
@@ -89,22 +79,22 @@ export default {
         nextPage: 1,
       },
       bannersList: [],
-      recommends:[]
+      recommends: [],
     };
   },
-  methods:{
+  methods: {
     getHomeMultidata() {
-      getHomeMultidata().then(res => {
+      getHomeMultidata().then((res) => {
         console.log(res);
         this.bannersList = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-        console.log(this.bannersList)
-      })
-    }
+        console.log(this.bannersList);
+      });
+    },
   },
   created() {
-    this.getHomeMultidata()
-  }
+    this.getHomeMultidata();
+  },
 };
 </script>
 
