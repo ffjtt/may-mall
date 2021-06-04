9ebddca28a532ee23f24afbef108d6153c74d17e
ffjtt <946917394@qq.com>
2021-06-04 17:54:00 +0800

    six commit

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  props: {
+    // probeType : {
+    //   type: Number,
+    //   default: 1
+    // },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      scroll: null
+    };
+  },
+  created() {},
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      // probeType: this.probeType,
+      click:true,
+      pullUpLoad: this.pullUpLoad,
+    });
+
+    // this.scroll.on('scroll',position => {
+    //   this.$emit('scroll',position)
+    // })
+
+    if(this.pullUpLoad) {
+      this.scroll.on('pullingUp',() => {
+        console.log('pullingUp');
+        this.$emit('pullingUp')
+      })
+    }
+  },
+  
+
+};
+</script>
+
+<style>
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,24 +4,18 @@
       <div slot="center">购物街</div>
     </nar-bar>
 
-    <home-swiper :banners="bannersList"></home-swiper>
-    <Recommend :recommend="recommendsList"></Recommend>
-    <feature></feature>
-    <tar-control class="tarcontrol" :title="['流行','新款','爆款']" @tarChange="tarChange"></tar-control>
-    <Goods :goodsList="pushData"></Goods>
-
-    <ul>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-      <li>zhanwei</li>
-    </ul>
+    <scroll :pullUpLoad="true"
+    @pullingUp="pullingup">
+      <home-swiper :banners="bannersList"></home-swiper>
+      <Recommend :recommend="recommendsList"></Recommend>
+      <feature></feature>
+      <tar-control
+        class="tarcontrol"
+        :title="['流行', '新款', '爆款']"
+        @tarChange="tarChange"
+      ></tar-control>
+      <Goods :goodsList="pushData"></Goods>
+    </scroll>
   </div>
 </template>
 
@@ -30,10 +24,11 @@ import NarBar from "components/common/navbar/NavBar";
 import HomeSwiper from "views/home/childcomps/HomeSwiper";
 import Recommend from "views/home/childcomps/Recommend";
 import Feature from "views/home/childcomps/Feature";
-import TarControl from 'components/content/tarcontrol/TarControl'
-import Goods from 'components/content/goods/Goods'
+import TarControl from "components/content/tarcontrol/TarControl";
+import Goods from "components/content/goods/Goods";
+import Scroll from "components/common/scroll/Scroll";
 
-import { getHomeMultidata,getHomeGoods } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 export default {
   name: "Home",
   components: {
@@ -42,61 +37,61 @@ export default {
     Feature,
     Recommend,
     TarControl,
-    Goods
-    
+    Goods,
+    Scroll,
   },
   data() {
     return {
       bannersList: [
-          {
-            acm: "3.mce.2_10_1jhwa.43542.0.ccy5br4OlfK0Q.pos_0-m_454801-sd_119",
-            height: 390,
-            height923: 390,
-            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
-            image923:
-              "https://s10.mogucdn.com/mlcdn/c45406/180926_7d5c521e0aa3h38786lkakebkjlh8_750x390.jpg",
-            link: "https://www.baidu.com",
-            title: "焕新女装节",
-            width: 750,
-            width923: 750,
-          },
-          {
-            acm: "3.mce.2_10_1ji16.43542.0.ccy5br4OlfK0R.pos_1-m_454889-sd_119",
-            height: 390,
-            height923: 390,
-            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
-            image923:
-              "https://s10.mogucdn.com/mlcdn/c45406/180926_14l41d2ekghbeh771g3ghgll54224_750x390.jpg",
-            link: "https://www.baidu.com",
-            title: "入秋穿搭指南",
-            width: 750,
-            width923: 750,
-          },
-          {
-            acm: "3.mce.2_10_1jfj8.43542.0.ccy5br4OlfK0S.pos_2-m_453270-sd_119",
-            height: 390,
-            height923: 390,
-            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
-            image923:
-              "https://s10.mogucdn.com/mlcdn/c45406/180919_47iclhel8f4ld06hid21he98i93fc_750x390.jpg",
-            link: "https://www.baidu.com",
-            title: "秋季护肤大作战",
-            width: 750,
-            width923: 750,
-          },
-          {
-            acm: "3.mce.2_10_1jepe.43542.0.ccy5br4OlfK0T.pos_3-m_452733-sd_119",
-            height: 390,
-            height923: 390,
-            image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
-            image923:
-              "https://s10.mogucdn.com/mlcdn/c45406/180917_0hgle1e2c350a57ekhbj4f10a6b03_750x390.jpg",
-            link: "https://www.baidu.com",
-            title: "流行抢先一步",
-            width: 750,
-            width923: 750,
-          },
-        ],
+        {
+          acm: "3.mce.2_10_1jhwa.43542.0.ccy5br4OlfK0Q.pos_0-m_454801-sd_119",
+          height: 390,
+          height923: 390,
+          image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
+          image923:
+            "https://s10.mogucdn.com/mlcdn/c45406/180926_7d5c521e0aa3h38786lkakebkjlh8_750x390.jpg",
+          link: "https://www.baidu.com",
+          title: "焕新女装节",
+          width: 750,
+          width923: 750,
+        },
+        {
+          acm: "3.mce.2_10_1ji16.43542.0.ccy5br4OlfK0R.pos_1-m_454889-sd_119",
+          height: 390,
+          height923: 390,
+          image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
+          image923:
+            "https://s10.mogucdn.com/mlcdn/c45406/180926_14l41d2ekghbeh771g3ghgll54224_750x390.jpg",
+          link: "https://www.baidu.com",
+          title: "入秋穿搭指南",
+          width: 750,
+          width923: 750,
+        },
+        {
+          acm: "3.mce.2_10_1jfj8.43542.0.ccy5br4OlfK0S.pos_2-m_453270-sd_119",
+          height: 390,
+          height923: 390,
+          image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
+          image923:
+            "https://s10.mogucdn.com/mlcdn/c45406/180919_47iclhel8f4ld06hid21he98i93fc_750x390.jpg",
+          link: "https://www.baidu.com",
+          title: "秋季护肤大作战",
+          width: 750,
+          width923: 750,
+        },
+        {
+          acm: "3.mce.2_10_1jepe.43542.0.ccy5br4OlfK0T.pos_3-m_452733-sd_119",
+          height: 390,
+          height923: 390,
+          image: "https://www.baidu.com/img/flexible/logo/pc/result.png",
+          image923:
+            "https://s10.mogucdn.com/mlcdn/c45406/180917_0hgle1e2c350a57ekhbj4f10a6b03_750x390.jpg",
+          link: "https://www.baidu.com",
+          title: "流行抢先一步",
+          width: 750,
+          width923: 750,
+        },
+      ],
       banners: [],
       recommends: [],
       recommendsList: [
@@ -104,8 +99,7 @@ export default {
           acm: "3.mce.2_10_1dggc.13730.0.ccy5br4OlfK0U.pos_0-m_313898-sd_119",
           image:
             "https://dss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=306547450,2932158275&fm=58",
-          link:
-            "http://www.baidu.com",
+          link: "http://www.baidu.com",
           sort: 1,
           title: "十点抢券",
         },
@@ -113,8 +107,7 @@ export default {
           acm: "3.mce.2_10_1dgge.13730.0.ccy5br4OlfK0V.pos_1-m_313899-sd_119",
           image:
             "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2681268939,2648248113&fm=58",
-          link:
-            "http://www.baidu.com",
+          link: "http://www.baidu.com",
           sort: 2,
           title: "好物特卖",
         },
@@ -122,8 +115,7 @@ export default {
           acm: "3.mce.2_10_1b610.13730.0.ccy5br4OlfK0W.pos_2-m_260486-sd_119",
           image:
             "https://dss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2479555558,3351257281&fm=58",
-          link:
-            "http://www.baidu.com",
+          link: "http://www.baidu.com",
           sort: 3,
           title: "内购福利",
         },
@@ -131,34 +123,33 @@ export default {
           acm: "3.mce.2_10_1dggg.13730.0.ccy5br4OlfK0X.pos_3-m_313900-sd_119",
           image:
             "https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2681268939,2648248113&fm=58",
-          link:
-            "http://www.baidu.com",
+          link: "http://www.baidu.com",
           sort: 4,
           title: "初秋上新",
         },
       ],
       goods: {
-        pop:{page:0,list:[]},
-        new:{page:0,list:[]},
-        sell:{page:0,list:[]}
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
       },
-      current:'pop'
+      current: "pop",
     };
   },
   methods: {
     tarChange(index) {
-      switch(index) {
-        case 0:{
-          this.current = 'pop'
-          break
+      switch (index) {
+        case 0: {
+          this.current = "pop";
+          break;
         }
-        case 1:{
-          this.current = 'new'
-          break
+        case 1: {
+          this.current = "new";
+          break;
         }
-        case 2:{
-          this.current = 'sell'
-          break
+        case 2: {
+          this.current = "sell";
+          break;
         }
       }
     },
@@ -172,26 +163,30 @@ export default {
     },
     getHomeGoods(type) {
       const page = this.goods[type].page + 1;
-      getHomeGoods(type,page).then((res) => {
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1
-        console.log(this.goods)
-      })
-      
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        console.log(this.goods);
+      });
+    },
+
+    pullingup() {
+      console.log('pullingup');
+      this.getHomeGoods(this.current)
     }
   },
   created() {
     this.getHomeMultidata();
 
-    this.getHomeGoods("pop")
-    this.getHomeGoods("new")
-    this.getHomeGoods("sell")
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
-  computed:{
+  computed: {
     pushData() {
-      return this.goods[this.current].list
-    }
-  }
+      return this.goods[this.current].list;
+    },
+  },
 };
 </script>
 
@@ -207,9 +202,9 @@ export default {
   right: 0;
   left: 0;
   top: 0;
-  z-index: 9; 
+  z-index: 9;
 }
-.tarcontrol{
+.tarcontrol {
   position: sticky;
   top: 44px;
 }
